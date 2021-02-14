31f71b30df079f668fbfd52322c921753d98491b
Curnan Reidy <curnan.reidy@spanishpoint.ie>
2021-02-14 13:58:23 +0000

    added api

---
+++ src/Api/DescribeExpression.cs
@@ -0,0 +1,25 @@
+using CronExpressionDescriptor;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System;
+
+namespace cron.Api
+{
+    public static class DescribeExpression
+    {
+        [FunctionName("DescribeExpression")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "DescribeExpression/{expression}")] HttpRequest req, string expression)
+        {
+            try
+            {
+                return new OkObjectResult(ExpressionDescriptor.GetDescription(expression));
+            }
+            catch (FormatException ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+        }
+    }
+}
